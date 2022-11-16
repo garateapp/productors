fda335350c438ce96db446fbae779754dc49716a
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-11-16 01:57:14 -0300

    update login

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -1,5 +1,5 @@
 <div class="min-h-screen flex flex-col sm:justify-center items-center sm:pt-14" style="background-image: url({{asset('image/bg_intranet.jpg')}});background-size: cover;box-shadow: inset 0 0 0 2000px rgba(0, 0, 0, 0.4);">
-    <div class="pt-12">
+    <div class="pt-10 mt-10">
         {{ $logo }}
     </div>
 
