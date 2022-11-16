a63cd20e28722c0524701a322df013a4f95f9989
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-11-16 01:40:20 -0300

    Register Color

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -47,7 +47,7 @@
             @endif
 
             <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-white hover:text-gray-900" href="{{ route('login') }}">
+                <a class="underline text-sm text-white hover:text-gray-400" href="{{ route('login') }}">
                     {{ __('Ya estas registrado?') }}
                 </a>
 
