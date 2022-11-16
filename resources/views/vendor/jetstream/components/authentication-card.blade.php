386c4b09f2a917f2c802078fe011ded0bc863e04
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-11-16 01:43:30 -0300

    margin login

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -3,7 +3,7 @@
         {{ $logo }}
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 shadow-md overflow-hidden sm:rounded-lg" style="background-color: rgb(0,0,0,0.5);">
+    <div class="w-full sm:max-w-md mt-6 mx-2 px-6 py-4 shadow-md overflow-hidden sm:rounded-lg" style="background-color: rgb(0,0,0,0.5);">
         {{ $slot }}
     </div>
 </div>
