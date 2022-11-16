53a0ecde8f6ae7853c9851de6d3264fc5491a810
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-11-16 01:35:50 -0300

    Register Update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -47,12 +47,12 @@
             @endif
 
             <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
+                <a class="underline text-sm text-white hover:text-gray-900" href="{{ route('login') }}">
+                    {{ __('Ya estas registrado?') }}
                 </a>
 
                 <x-jet-button class="ml-4">
-                    {{ __('Register') }}
+                    {{ __('Registrarme') }}
                 </x-jet-button>
             </div>
         </form>
