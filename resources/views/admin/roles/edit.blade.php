66a4cafa9f43a90b757953e2f07fa1716679fbab
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-02-15 20:02:44 -0300

    u

--- resources/views/admin/roles/edit.blade.php
+++ resources/views/admin/roles/edit.blade.php
@@ -26,7 +26,7 @@
 
                 
             <div class="flex justify-center mt-6">
-                <button  class="items-center focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:mt-0 px-6 py-3 bg-blue-600 hover:bg-blue-500 focus:outline-none rounded">
+                <button  class="items-center focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:mt-0 px-6 py-3 bg-gray-500 hover:bg-gray-500 focus:outline-none rounded">
                     <p class="text-sm font-medium leading-none text-white">Crear Rol</p>
                 </button>
             </div>
