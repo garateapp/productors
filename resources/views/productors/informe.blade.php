319d5d14366857a690066dd2d95abaafbba76ed1
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-02-27 15:36:48 -0300

    u

--- resources/views/productors/informe.blade.php
+++ resources/views/productors/informe.blade.php
@@ -29,7 +29,7 @@
 </head>
 <body>
 	<div class="container">
-		<img src="ruta/a/tu/logo.png" alt="Logo de la empresa" class="logo">
+		<img src="https://static.wixstatic.com/media/08547c_e7dc5092cad4472189d3be634557e720~mv2.png/v1/fill/w_314,h_89,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/GREENEX_logo.png" alt="Logo de la empresa" class="logo">
 		<h1>Reporte de la empresa</h1>
 		<!-- Aquí puedes incluir tus gráficos en formato HTML -->
 		<!-- Ejemplo: <div id="grafico"></div> -->
