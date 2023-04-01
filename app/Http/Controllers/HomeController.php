a51870e6abf00a2de77afeafe07c9c7d68322a16
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-04-01 14:43:25 -0300

    u

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -55,7 +55,7 @@ class HomeController extends Controller
  
          $pdf = PDF::loadView('productors.informe', ['recepcion' => $recepcion]);
  
-         return $pdf->download('reporte Nro:'.$recepcion->id.'.pdf');
+         return $pdf->download($recepcion->id_g_recepcion.'-'.$recepcion->id_emisor);
     }
 
     public function production()
