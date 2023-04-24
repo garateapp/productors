a81e4aba84fc5c791db3a7697d0aa25d5a352a55
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-04-24 17:42:26 -0400

    u

--- resources/views/livewire/admin/inbox.blade.php
+++ resources/views/livewire/admin/inbox.blade.php
@@ -26,20 +26,7 @@
                 </a>
              @endif
            
-            <a href="#" class="w-16 p-4 border text-gray-700 rounded-2xl mb-24">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1"
-                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-              </svg>
-            </a>
-            <a href="#" class="w-16 p-4 border text-gray-700 rounded-2xl">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1"
-                  d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1"
-                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-              </svg>
-            </a>
+           
           </nav>
         </section>
         <section class="hidden sm:flex flex-col pt-3 w-full sm:w-4/12 bg-gray-50 h-full overflow-y-scroll order-2 lg:order-1">
@@ -116,7 +103,7 @@
               <section>
                 <h1 class="font-bold text-2xl">{{$current->tipo}} Para Productores De {{$current->especie}}</h1>
                 <article class="mt-8 text-gray-500 leading-7 tracking-wider">
-                  <p>{{$current->observacion}}</p>
+                  <p>{!!$current->observacion!!}</p>
                  
                   <footer class="mt-12">
                     <p>Atte. Administración</p>
