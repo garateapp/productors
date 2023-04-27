9bc9e25da771aebb878fe4750b939dad305bb5b1
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-04-27 01:25:51 -0400

    u

--- resources/views/livewire/procesos/proceso-especie.blade.php
+++ resources/views/livewire/procesos/proceso-especie.blade.php
@@ -127,6 +127,7 @@
                       <th class="text-center">%<br>Comercial</th>
                       <th class="text-center">%<br>Desecho</th>
                       <th class="text-center">%<br>Merma</th>
+                      <th class="text-center">Informe</th>
                       <th> </th>
                       
                       
@@ -271,49 +272,49 @@
                             </td>
                                   
                             <td class="pl-5">
+                                          
+                                       <div class="content-center">
+                                          @if ($proceso->informe)
+                                             <a href="{{route('download.proceso',$proceso)}}" target="_blank" class="h-10 m-2 w-full mr-2 items-center content-center">   
+                                                <img class="h-10 m-2 w-full mr-2" src="{{asset('image/pdf_icon2.png')}}" title="Descargar" alt="">
+                                             </a>
+                                          
+                                             
+                                          @else
+                                             
+                                          @endif
+                                       
+                                       
+                                       </div>                            
                                  
-                              <div class="content-center">
-                                 @if ($proceso->informe)
-                                    <a href="{{route('download.proceso',$proceso)}}" target="_blank" class="h-10 m-2 w-full mr-2 items-center content-center">   
-                                       <img class="h-10 m-2 w-full mr-2" src="{{asset('image/pdf_icon2.png')}}" title="Descargar" alt="">
-                                    </a>
-                                  
-                                    
-                                 @else
-                                    
-                                 @endif
                               
+                                 
                               
-                              </div>                            
-                        
-                     
-                        
-                     
-                        </td>
-                        <td class="pl-5">
-                        
-                           <div class="block w-full">
-                              @if ($proceso->informe)
-                                
-                                 <form action="{{route('delete.proceso',$proceso)}}" method="POST">
-                                    @csrf
-                                    @method('delete')
-                  
-                                    <button class="font-bold py-1 px-3 rounded-full bg-red-500 text-white text-xl" type="submit" title="Eliminar">x</button>
+                                 </td>
+                                 <td class="pl-5">
+                                 
+                                          <div class="block w-full">
+                                             @if ($proceso->informe)
+                                             
+                                                <form action="{{route('delete.proceso',$proceso)}}" method="POST">
+                                                   @csrf
+                                                   @method('delete')
+                                 
+                                                   <button class="font-bold py-1 px-3 rounded-full bg-red-500 text-white text-xl" type="submit" title="Eliminar">x</button>
+                                                   
+                                             </form>
+                                                
+                                             @else
+                                                
+                                             @endif
+                                          
+                                          
+                                          </div>                            
                                     
-                              </form>
                                  
-                              @else
+                                    
                                  
-                              @endif
-                           
-                           
-                           </div>                            
-                     
-                  
-                     
-                  
-                        </td>
+                                    </td>
                                
                                   
                                
