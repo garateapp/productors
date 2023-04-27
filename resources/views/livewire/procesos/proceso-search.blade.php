c373f78cd2168d0c674dbdbbdbf7c1545ff0fb9c
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-04-27 01:02:02 -0400

    u

--- resources/views/livewire/procesos/proceso-search.blade.php
+++ resources/views/livewire/procesos/proceso-search.blade.php
@@ -280,7 +280,7 @@
                                  
                                        <div class="block w-full">
                                           @if ($proceso->informe)
-                                             <a href="{{route('download.proceso',$proceso)}}" target="_blank" class="h-10 m-2 w-full mb-2 mt-4 mr-2">   
+                                             <a href="{{route('download.proceso',$proceso)}}" target="_blank" class="h-10 m-2 w-full mt-4 mr-2">   
                                                 <img class="h-10 m-2 w-full mr-2" src="{{asset('image/pdf_icon2.png')}}" title="Descargar" alt="">
                                              </a>
                                            
