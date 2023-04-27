27415cbf2f3871398715de0814e33d94bc8576fd
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-04-27 00:48:47 -0400

    u

--- resources/views/livewire/procesos/proceso-search.blade.php
+++ resources/views/livewire/procesos/proceso-search.blade.php
@@ -282,13 +282,7 @@
                                              <a href="{{route('download.proceso',$proceso)}}" target="_blank" >   
                                                 <img class="h-10 objet-contain my-2 mr-2" src="{{asset('image/pdf_icon2.png')}}" title="Descargar" alt="">
                                              </a>
-                                             <form action="{{route('delete.proceso',$proceso)}}" method="POST">
-                                                @csrf
-                                                @method('delete')
-                              
-                                                <button class="font-bold py-1 px-3 mt-2 rounded-full bg-red-500 text-white text-2xl" type="submit" title="Eliminar">x</button>
-                                                
-                                          </form>
+                                            
                                              
                                           @else
                                              
@@ -300,6 +294,21 @@
                               
                                  
                               
+                                 </td>
+                                 <td>
+                                    @if ($proceso->informe)
+                                   
+                                    <form action="{{route('delete.proceso',$proceso)}}" method="POST">
+                                       @csrf
+                                       @method('delete')
+                     
+                                       <button class="font-bold py-1 px-3 mt-2 rounded-full bg-red-500 text-white text-2xl" type="submit" title="Eliminar">x</button>
+                                       
+                                 </form>
+                                    
+                                 @else
+                                    
+                                 @endif
                                  </td>
                               
                                  
