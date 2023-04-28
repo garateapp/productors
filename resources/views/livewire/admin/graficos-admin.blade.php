c7f60616216d7b80958dc50bed18fbaa655d5fc3
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-04-28 17:56:22 -0400

    u

--- resources/views/livewire/admin/graficos-admin.blade.php
+++ resources/views/livewire/admin/graficos-admin.blade.php
@@ -58,10 +58,10 @@
               </div>
               <div class="flex items-center">
                  
-                 <h1 class="block my-2 text-xl font-bold">{{number_format(77)}}%</h1>
+                 <h1 class="block my-2 text-xl font-bold">{{number_format($exp_total*100/$cant)}}%</h1>
                     <div class="relative py-2 w-full mx-4">
                        <div class="w-full overflow-hidden h-4 text-4xl flex rounded bg-gray-200">
-                         <div style="width: 77%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500 transition-all duration-500">
+                         <div style="width: {{$exp_total*100/$cant}}%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500 transition-all duration-500">
                            </div>
                        </div>
                    </div>
