b1d8faae34aecb2b7176f7c8d08b5c05e5328f22
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-05-23 17:55:58 -0400

    u

--- resources/views/productors/informe.blade.php
+++ resources/views/productors/informe.blade.php
@@ -213,7 +213,7 @@
 				$c=$recepcion->calidad->detalles->where('tipo_item','COLOR DE CUBRIMIENTO')->first()->cantidad;
 			}
 			@endphp
-			{{number_format($recepcion->peso_neto*($total+$a+$b+$c/100,0))}} Kgs
+			{{number_format($recepcion->peso_neto*($total+$a+$b+$c/100))}} Kgs
 			
 				
 		</td>
