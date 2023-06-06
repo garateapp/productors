159688b5d82ad36462d0c57128ba95089a5195a9
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-06-06 11:31:25 -0400

    y

--- resources/views/livewire/calidad/agregar-cc.blade.php
+++ resources/views/livewire/calidad/agregar-cc.blade.php
@@ -388,7 +388,7 @@
                                                     @if ($detalle->porcentaje_muestra)
                                                         {{$detalle->porcentaje_muestra}}
                                                     @endif
-                                                @elseif($recep->n_especie=='Orange' || $recep->n_especie=='Peaches' || $recep->n_especie=='Apples' || $recep->n_especie=='Pears' || $recep->n_especie=='Nectarines')
+                                                @elseif($recep->n_especie=='Orange' || $recep->n_especie=='Peaches' || $recep->n_especie=='Apples' || $recep->n_especie=='Pears' || $recep->n_especie=='Nectarines' || $recep->n_especie=='Plums')
                                                     @if ($detalle->porcentaje_muestra)
                                                         {{$detalle->porcentaje_muestra}}
                                                     @endif
