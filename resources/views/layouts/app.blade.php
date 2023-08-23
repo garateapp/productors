b43c2f03af6ac27fa66c8a42db14119611de959a
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-22 21:22:13 -0400

    u

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,8 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        
+        <link rel="shortcut icon" href="{{asset('image/logogreenex.png')}}">
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
