94dafaa0dfc3bf0eae8707cb7b1e7ab5b7a8676c
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-22 21:25:54 -0400

    u

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <link rel="shortcut icon" href="{{asset('image/logogreenex.png')}}">
+        <link rel="shortcut icon" href="{{asset('image/iconogreenex.png')}}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
