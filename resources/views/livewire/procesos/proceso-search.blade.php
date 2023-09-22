559f0012bbb579d07b9444def483f33fe3c8a4a4
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-09-22 12:18:47 -0300

    u

--- resources/views/livewire/procesos/proceso-search.blade.php
+++ resources/views/livewire/procesos/proceso-search.blade.php
@@ -1,4 +1,15 @@
 <div>
+   @if (session('info'))
+      <div x-data="{open: true}">
+         <div x-show="open"  class="bg-white border border-gray-400 text-gray-700 px-4 py-3 rounded relative" role="alert">
+            <strong class="font-bold">Felicidades!</strong>
+            <span class="block sm:inline">{{session('info')}}</span>
+            <span x-on:click="open=false" class="absolute top-0 bottom-0 right-0 px-4 py-3">
+            <svg class="fill-current h-6 w-6 text-gray-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+            </span>
+         </div>
+      </div>
+   @endif
    <script src="https://code.highcharts.com/highcharts.js"></script>
    <script src="https://code.highcharts.com/modules/series-label.js"></script>
    <script src="https://code.highcharts.com/modules/exporting.js"></script>
@@ -118,17 +129,7 @@
                      
          <div class="bg-white py-4 md:py-7 px-4 md:px-8 xl:px-10 ">
 
-            @if (session('info'))
-                  <div x-data="{open: true}">
-                     <div x-show="open"  class="bg-white border border-gray-400 text-gray-700 px-4 py-3 rounded relative" role="alert">
-                        <strong class="font-bold">Felicidades!</strong>
-                        <span class="block sm:inline">{{session('info')}}</span>
-                        <span x-on:click="open=false" class="absolute top-0 bottom-0 right-0 px-4 py-3">
-                        <svg class="fill-current h-6 w-6 text-gray-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
-                        </span>
-                     </div>
-                  </div>
-            @endif
+         
             
             <x-table-responsive>   
                <table class="min-w-full divide-y divide-gray-200 mb-20 pb-20">
