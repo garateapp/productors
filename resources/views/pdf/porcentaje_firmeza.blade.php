c82dad25395d1ef82d59f6f6debbf760c58bc81f
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-11-09 23:58:05 -0300

    f

--- resources/views/pdf/porcentaje_firmeza.blade.php
+++ resources/views/pdf/porcentaje_firmeza.blade.php
@@ -263,7 +263,7 @@
                     format: '{point.y:.1f}%'
                 },
                 pointPadding: 0.01,
-                borderWidth: 0.9
+                borderWidth: 1.1
             }
         },
             series: [
