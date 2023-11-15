81ff660d2467dec5e4d599fe1625fd21daa0a890
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-11-15 11:06:53 -0300

    u

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -88,7 +88,8 @@ class HomeController extends Controller
             ]);
             //luego se busca al productor que tiene el nombre de la agricola del proceso
             $user=User::where('name',$proceso->agricola)->first();
-            if($user){
+
+            if(!is_null($user)){
                     //en caso que exista el usuarioo consultar si tiene telefonos registrados
                     if($user->telefonos->count()){
                         foreach($user->telefonos as $telefono){
