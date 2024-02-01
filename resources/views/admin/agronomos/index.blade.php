de5bed45c3852f991ca7b36433d82a9b31a6a0f2
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-02-01 16:27:21 -0300

    u

--- resources/views/admin/agronomos/index.blade.php
+++ resources/views/admin/agronomos/index.blade.php
@@ -38,7 +38,7 @@
         </h1>
         
       <h1 class="text-center">
-      Nombre/Apellido  / Nro Productores asignados / Nro Productores finalizados 
+      Nombre/Apellido  / Nro Productores asignados / Nro Productores finalizados / Nro Productores pendientes 
         </h1>
       
     </div>
