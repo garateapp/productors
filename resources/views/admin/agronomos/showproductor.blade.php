c31b51ab48541234aec7dc6466ff0bb67a2e09cf
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-02-12 23:49:06 -0300

    u

--- resources/views/admin/agronomos/showproductor.blade.php
+++ resources/views/admin/agronomos/showproductor.blade.php
@@ -122,7 +122,7 @@
                           <path fill-rule="evenodd" d="M12.395 2.553a1 1 0 00-1.45-.385c-.345.23-.614.558-.822.88-.214.33-.403.713-.57 1.116-.334.804-.614 1.768-.84 2.734a31.365 31.365 0 00-.613 3.58 2.64 2.64 0 01-.945-1.067c-.328-.68-.398-1.534-.398-2.654A1 1 0 005.05 6.05 6.981 6.981 0 003 11a7 7 0 1011.95-4.95c-.592-.591-.98-.985-1.348-1.467-.363-.476-.724-1.063-1.207-2.03zM12.12 15.12A3 3 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879A2.99 2.99 0 0113 13a2.99 2.99 0 01-.879 2.121z" clip-rule="evenodd"></path>
                         </svg>
                         <p class="">
-                          17 Datos Pendientes
+                          RUT: {{$user->rut}}
                         </p>
                       </div>
                     </div>
@@ -157,14 +157,57 @@
                     @enderror
                 </div>
               </div>
-              <div class="form-group mt-4">
+              <div class="form-group mt-2">
                   {!! Form::label('exportadora','Exportadora') !!}
-                  {!! Form::text('exportadora', null , ['class'=>'mt-1 block w-full rounded-lg', 'placeholder'=>'']) !!}
+                  {!! Form::text('exportadora', 'Greenex' , ['class'=>'mt-1 block w-full rounded-lg',  'readonly' => 'readonly' ,  'placeholder'=>'']) !!}
                   
                   @error('exportadora')
                       <span class="text-danger">{{$message}}</span>
                   @enderror
               </div>
+              <div class="form-group mt-2">
+                {!! Form::label('predio','Predio') !!}
+                {!! Form::text('predio', null , ['class'=>'mt-1 block w-full rounded-lg', 'placeholder'=>'']) !!}
+                
+                @error('predio')
+                    <span class="text-danger">{{$message}}</span>
+                @enderror
+              </div>
+              <div class="form-group mt-2">
+                {!! Form::label('cuartel','Cuartel') !!}
+                {!! Form::text('cuartel', null , ['class'=>'mt-1 block w-full rounded-lg', 'placeholder'=>'']) !!}
+                
+                @error('cuartel')
+                    <span class="text-danger">{{$message}}</span>
+                @enderror
+              </div>
+              <div class="form-group mt-2">
+               <h1 class="text-center">Especie:</h1>
+                <div class="grid grid-cols-6 gap-y-2">
+
+                  @foreach ($user->especies_comercializas()->get() as $especie)
+                      <div class="flex justify-center">
+                          <button class="py-3 px-3 text-sm focus:outline-none leading-none text-green-700 bg-green-100 rounded">{{$especie->name}}</button>
+                      </div>
+                  @endforeach
+
+                </div>
+                
+              </div>
+              <div class="form-group mt-2">
+                <h1 class="text-center">Variedades:</h1>
+                 <div class="grid grid-cols-6 gap-y-2">
+ 
+                   @foreach ($user->variedades_comercializas()->get() as $especie)
+                       <div class="flex justify-center">
+                           <button class="py-3 px-3 text-sm focus:outline-none leading-none text-green-700 bg-green-100 rounded">{{$especie->name}}</button>
+                       </div>
+                   @endforeach
+ 
+                 </div>
+                 
+               </div>
+
 
 
               <div class="flex justify-center mt-4">
