18bcfab4f09f284fbfe961d44639760004866575
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-03-14 17:38:24 -0300

    u

---
+++ app/Http/Livewire/Agronomo/FichaList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Agronomo;
+
+use Livewire\Component;
+
+class FichaList extends Component
+{
+    public function render()
+    {
+        return view('livewire.agronomo.ficha-list');
+    }
+}
--- resources/views/admin/agronomos/editproductor.blade.php
+++ resources/views/admin/agronomos/editproductor.blade.php
@@ -504,15 +504,15 @@
                                       <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
                                     </svg>
                                   
-                                    <form action="{{route('fichas.destroy',$ficha)}}" method="POST">
-                                      @csrf
-                                      @method('delete')
-                                    <button>
-                                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6 text-red-400 hover:text-red-800 ml-2">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                      </svg>
-                                    </button>
-                                  </form>
+                                    <form id="deleteForm" action="{{ route('fichas.destroy', $ficha) }}" method="POST">
+                                        @csrf
+                                        @method('delete')
+                                        <button type="button" id="deleteButton">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6 text-red-400 hover:text-red-800 ml-2">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                            </svg>
+                                        </button>
+                                    </form>
                                   
                                     
                                   </div>
@@ -529,6 +529,8 @@
                         @endphp
                     @endforeach
                   </div>
+
+                  
 
                 @endif
                 @php
@@ -690,6 +692,27 @@
         porcentajeEntregaInput.addEventListener('input', calcularKilosEntregables);
     });
 </script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        document.getElementById('deleteButton').addEventListener('click', function () {
+            Swal.fire({
+                title: "¿Eliminar temporada?",
+                text: "No podrás revertir esta acción.",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Sí, eliminarla"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    document.getElementById('deleteForm').submit();
+                }
+            });
+        });
+    });
+</script>
+
 
 
  
