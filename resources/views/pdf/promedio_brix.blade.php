1690895736513f8970d54bc487cc8be9daa107e9
calgreenex <carlos.alvarez@greenex.cl>
2024-12-20 11:28:50 -0300

    error highcharts.js

--- resources/views/pdf/promedio_brix.blade.php
+++ resources/views/pdf/promedio_brix.blade.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 	<title>Informe de Recepción Nro° {{$recepcion->numero_g_recepcion}}</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<link href=”https://fonts.googleapis.com/css?family=Pacifico” rel=”stylesheet”>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+    {{-- <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> --}}
 	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <!-- Correcto -->
+<script src="https://code.highcharts.com/highcharts.js" defer></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://code.highcharts.com/modules/series-label.js"></script>
     <script src="https://code.highcharts.com/modules/exporting.js"></script>
     <script src="https://code.highcharts.com/modules/export-data.js"></script>
@@ -20,15 +23,15 @@
 </head>
 <body>
 
-    <figure class="highcharts-figure mx-1 mt-4">
+    <figure class="mx-1 mt-4 highcharts-figure">
         <div id="container">
-           
+
         </div>
      </figure>
 
 
-    
-	
+
+
     @php
         $categories=[];
         $series=[];
@@ -38,15 +41,15 @@
         } else {
             $items=['LIGHT','DARK','BLACK'];
         }
-        
-       
+
+
     @endphp
 
     @foreach ($items as $item)
         @if ($recepcion->n_variedad=='Dagen')
             @if ($recepcion->calidad->detalles->where('tipo_item','BRIX DAGEN')->where('detalle_item',$item)->count()>0)
                 @foreach ($recepcion->calidad->detalles->where('tipo_item','BRIX DAGEN')->where('detalle_item',$item) as $detalle)
-                    
+
                         @php
                             $categories[]=$detalle->detalle_item;
                             $series[]=$detalle->valor_ss;
@@ -60,8 +63,8 @@
                                     }
                                 }
                         @endphp
-                        
-                    
+
+
                 @endforeach
             @else
                         @php
@@ -69,16 +72,16 @@
                             $series[]=0;
                         @endphp
             @endif
-            
+
         @else
             @if ($recepcion->calidad->detalles->where('tipo_item','SOLIDOS SOLUBLES')->where('detalle_item',$item)->count()>0)
                 @foreach ($recepcion->calidad->detalles->where('tipo_item','SOLIDOS SOLUBLES')->where('detalle_item',$item) as $detalle)
-                    
+
                         @php
                             $categories[]=$detalle->detalle_item;
                             $series[]=$detalle->valor_ss;
                         @endphp
-                    
+
                 @endforeach
             @else
                         @php
@@ -88,26 +91,28 @@
             @endif
         @endif
     @endforeach
-                    
+
     @if ($recepcion->n_especie=='Cherries')
         @php
             $colors=['#800000','#400000','#000000'];
         @endphp
     @elseif($recepcion->n_variedad=='Dagen')
         @php
-            
+
         @endphp
-    @else 
+    @else
         @php
             $colors=['#600000','#400000','#000000'];
         @endphp
     @endif
+
     <script>
+       $(document).ready(function() {
         var categories = <?php echo json_encode($categories) ?>;
         var series = <?php echo json_encode($series) ?>;
         var col = <?php echo json_encode($colors) ?>;
 
-                Highcharts.chart('container', {
+         Highcharts.chart('container', {
             chart: {
                 type: 'column'
             },
@@ -156,6 +161,7 @@
 
             }]
         });
+    });
       </script>
 </body>
 </html>
