61658148b7fc691c168e2573ec1b67950113a266
calgreenex <carlos.alvarez@greenex.cl>
2025-02-20 10:18:46 -0300

    .

--- resources/views/pdf/promedio_firmeza.blade.php
+++ resources/views/pdf/promedio_firmeza.blade.php
@@ -121,7 +121,7 @@
                     }]
                 },
                 options: {
-                    aspectRatio: 1, // Ratio directo en Chart.js (sobrescribe CSS)
+                    aspectRatio: 16/9, // Ratio directo en Chart.js (sobrescribe CSS)
     responsive: true,
     maintainAspectRatio: true, // Activa el cálculo automático
                     plugins: {
